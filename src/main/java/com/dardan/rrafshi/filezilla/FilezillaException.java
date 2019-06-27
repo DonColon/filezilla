002d1f4b8a63e25f41282772b249ff10d42dd9c5
DonColon <drrafshi@hotmail.de>
2019-06-28 00:11:16 +0200

    Implemented FilezillaException class

---
+++ src/main/java/com/dardan/rrafshi/filezilla/FilezillaException.java
@@ -0,0 +1,52 @@
+package com.dardan.rrafshi.filezilla;
+
+import com.dardan.rrafshi.commons.exceptions.ApplicationException;
+
+
+public final class FilezillaException
+{
+	private FilezillaException() {}
+
+
+	public static class NoConnection extends ApplicationException
+	{
+		private static final long serialVersionUID = 1L;
+
+
+		public NoConnection(final String message, final Throwable cause) {
+			super(message, cause);
+		}
+
+		public NoConnection(final String message) {
+			super(message);
+		}
+	}
+
+	public static class DownloadFailed extends ApplicationException
+	{
+		private static final long serialVersionUID = 1L;
+
+
+		public DownloadFailed(final String message, final Throwable cause) {
+			super(message, cause);
+		}
+
+		public DownloadFailed(final String message) {
+			super(message);
+		}
+	}
+
+	public static class UploadFailed extends ApplicationException
+	{
+		private static final long serialVersionUID = 1L;
+
+
+		public UploadFailed(final String message, final Throwable cause) {
+			super(message, cause);
+		}
+
+		public UploadFailed(final String message) {
+			super(message);
+		}
+	}
+}
