cda583bedd7d5662103c8df16f9446498af24727
DonColon <dardan.rrafshi@gmail.com>
2020-08-10 04:59:30 +0200

    Added github package registry

--- src/main/java/com/dardan/rrafshi/filezilla/examples/ListFiles.java
+++ src/main/java/com/dardan/rrafshi/filezilla/examples/ListFiles.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.dardan.rrafshi.commons.crypto.Passwords;
+import com.dardan.rrafshi.commons.security.Passwords;
 import com.dardan.rrafshi.filezilla.Constants;
 import com.dardan.rrafshi.filezilla.FilezillaManager;
 import com.dardan.rrafshi.filezilla.model.FilezillaPath;
--- src/main/java/com/dardan/rrafshi/filezilla/examples/MoveFile.java
+++ src/main/java/com/dardan/rrafshi/filezilla/examples/MoveFile.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.dardan.rrafshi.commons.crypto.Passwords;
+import com.dardan.rrafshi.commons.security.Passwords;
 import com.dardan.rrafshi.filezilla.Constants;
 import com.dardan.rrafshi.filezilla.FilezillaManager;
 import com.dardan.rrafshi.filezilla.model.FilezillaPath;
